a30c8a9ade2efdcc18da2f60d5b0632702344b88
jasonhawkharris <jasonhawkharris@gmail.com>
2020-10-29 11:52:26 -0700

    More setup.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Hello(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='sidebar'>sidebar</div>
+      <div className='content'>content</div>
+      <div className='todo-list'>todo-list</div>
     </div>
   );
 }
 
-export default App;
+export default Hello;
