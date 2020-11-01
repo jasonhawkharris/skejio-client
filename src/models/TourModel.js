a5fe39676fb0f9646bb1f01fc9e9011be3046a33
jasonhawkharris <jasonhawkharris@gmail.com>
2020-11-01 12:14:37 -0800

    Creates tourModel for front end

---
+++ src/models/TourModel.js
@@ -0,0 +1,33 @@
+const URL = 'http://localhost:3001/api/v1/tours';
+
+class TourModel {
+    static async all() {
+        try {
+            const response = await fetch(URL, {
+                method: 'GET',
+                headers: {
+                    authentication: `Bearer ${localStorage.uid}`,
+                }
+            });
+            return await response.json();
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    static async show(id) {
+        try {
+            const response = await fetch(`${URL}/${id}`, {
+                method: 'GET',
+                headers: {
+                    authentication: `Bearer ${localStorage.uid}`,
+                }
+            });
+            return await response.json();
+        } catch (err) {
+            console.log(err);
+        }
+    }
+}
+
+export default TourModel;
