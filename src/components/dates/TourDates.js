30fd102d8bd5faad3347ce788cc49e09fe6644d6
jasonhawkharris <jasonhawkharris@gmail.com>
2020-11-03 15:33:46 -0800

    Dates page items now link to individual date show pages

--- src/components/dates/TourDates.js
+++ src/components/dates/TourDates.js
@@ -1,16 +1,20 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React from 'react';
-
+import { NavLink } from 'react-router-dom';
 
 import TourDateCard from './TourDateCard';
-
 import './Dates.css';
+
 
 const TourDates = props => {
     const generateTourDateCards = tourDates => {
         try {
             return tourDates.map(tourDate => {
-                return <TourDateCard key={tourDate._id} tourDate={tourDate} />
+                return (
+                    <NavLink to={`/tour-date/${tourDate._id}`}>
+                        <TourDateCard key={tourDate._id} tourDate={tourDate} />
+                    </NavLink >
+                )
             });
         } catch (error) {
             console.log(error);
