c754c04d2ccc93dfcfbd509a0b2aefa117a29717
jasonhawkharris <jasonhawkharris@gmail.com>
2020-11-25 08:45:33 -0800

    resets the code base but keeps all dependencies.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect } from 'react';
-import Home from './pages/Home';
-
-import { useSetRecoilState } from 'recoil';
-import { userState } from './recoil/atoms';
-
-import UserModel from './models/UserModel';
+import React from 'react';
 
 import './App.css';
 
 function App(props) {
-	const setUser = useSetRecoilState(userState);
-
-	useEffect(function () {
-		if (localStorage.uid) {
-			UserModel.show().then(response => {
-				console.log(response);
-				setUser(response.user);
-			})
-		}
-	}, [])
-
 	return (
-		<div className='App'>
-			<Home />
-		</div>
-	);
+		<h1>This is the main app.</h1>
+	)
 }
 
 export default App;
