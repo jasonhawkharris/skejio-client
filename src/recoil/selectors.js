b581d0fa4e7feb6edd2d673fd03ea9222eed121e
jasonhawkharris <jasonhawkharris@gmail.com>
2020-11-25 09:11:13 -0800

    completes selector

--- src/recoil/selectors.js
+++ src/recoil/selectors.js
@@ -1,6 +1,11 @@
+import { selector } from 'recoil';
 import { userState } from './atoms';
 
 export const loggedInState = selector({
     key: 'loggedInState',
-    get: ({ get }) => get(userState);
-})
+    get: ({ get }) => {
+        const user = get(userState);
+        if (user) return true;
+        return false;
+    },
+});
