1e07b8e289f5a41b1dab6f99c373324075d6c2b9
jasonhawkharris <jasonhawkharris@gmail.com>
2020-11-25 15:07:08 -0800

    completes first draft of team page.

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -1,8 +1,60 @@
 import React from 'react';
+import { useRecoilValue } from 'recoil';
+import { userState } from '../../recoil/atoms';
+
+import './Team.css';
 
 const Team = props => {
+    const user = useRecoilValue(userState);
+    const manager = user.manager;
+    const agent = user.agent;
+
+    const generateTeammates = teamMember => {
+        return teamMember.teammates.map(teammate => {
+            return (
+                <>{teammate.firstName} { teammate.lastName}</>
+            )
+        })
+    }
+
     return (
-        <h1>TEAM</h1>
+        <div>
+            <h1>My Team</h1>
+            <div className="ui inverted raised segment">
+                <div className="teammate-card">
+                    <div>
+                        <img className="avatar" src="https://i.pravatar.cc/150?img=68" alt="avatar" />
+                    </div>
+                    <div>
+                        <h3>{manager.__t}</h3>
+                        <div>
+                            <strong>Name:</strong> {manager.firstName} {manager.lastName} <br />
+                            <strong>Email:</strong> {manager.email} <br />
+                            <strong>Company:</strong> {manager.company.name} <br />
+                            <strong>Teammates:</strong> {generateTeammates(manager)}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <br />
+            <div className="ui inverted raised segment">
+                <div className="teammate-card">
+                    <div>
+                        <img className="avatar" src="https://i.pravatar.cc/150?img=60" alt="avatar" />
+                    </div>
+                    <div>
+                        <h3>{agent.__t}</h3>
+                        <div>
+                            <strong>Name:</strong> {agent.firstName} {agent.lastName} <br />
+                            <strong>Email:</strong> {agent.email} <br />
+                            <strong>Company:</strong> {agent.agency.name} <br />
+                            <strong>Teammates:</strong> {generateTeammates(agent)}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     )
 }
 
