ef70f5fb3c628102574b8f7eff193d42e25aac76
jasonhawkharris <jasonhawkharris@gmail.com>
2020-12-08 10:33:10 -0800

    Need to revisit the UI, which is confusing at the moment. However, checkbox for completed todo is now active.

---
+++ src/components/TodoList/Checkbox.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Formik, Field, Form } from 'formik';
+
+import TodoModel from '../../models/TodoModel';
+import './Checkbox.css';
+
+const Checkbox = props => {
+    return (
+        <Formik
+            initialValues={{
+                completed: props.todo.completed ? true : false,
+            }}
+            onSubmit={async (values) => {
+                TodoModel.update(props.todo._id, values)
+                    .then(response => {
+                        console.log(response);
+                        props.fetch()
+                    })
+            }}
+        >
+            {({ values }) => (
+                <Form>
+                    <label>
+                        <Field
+                            type="checkbox"
+                            name="completed"
+                            style={{
+                                marginLeft: '10px',
+                                marginRight: '10px'
+                            }}
+                        />
+                    </label>
+                    <button className="ui mini compact icon pink button" type="submit">
+                        <i className="send icon"></i>
+                    </button>
+                </Form>
+            )}
+        </Formik>
+    )
+}
+
+export default Checkbox;
--- src/components/TodoList/Todo.jsx
+++ src/components/TodoList/Todo.jsx
@@ -1,33 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { formatDate } from '../../utils/helpers';
+import Checkbox from './Checkbox';
 import './TodoList.css';
 
 const Todo = props => {
     const todo = props.todo;
+    const [loading, setLoading] = useState(true);
+
+    useEffect(function () {
+        setTimeout(() => {
+            setLoading(false);
+        }, 1000);
+    }, []);
 
     return (
         <div className="ui inverted segment">
-            <div className="item">
-                <strong>Todo: </strong>{todo.content}
-            </div>
-            <div className="item">
-                <strong>Due By: </strong>{formatDate(todo.dueDate, true, true)}
-            </div>
-            <div className="item">
-                <strong>Created By: </strong>{todo.createdBy.firstName} {todo.createdBy.lastName}
-            </div>
-            <div className="item">
-                {/**The check box for completion must go here. */}
-            </div>
-            <div className="ui inverted mini basic icon buttons" style={{ marginTop: '20px' }}>
-                <button className="ui button">
-                    <i className="edit icon"></i>
-                </button>
-                <button className="ui button">
-                    <i className="trash icon"></i>
-                </button>
-            </div>
+            {!loading ? (
+                <>
+                    <div className="item">
+                        <strong>Todo: </strong>{todo.content}
+                    </div>
+                    <div className="item">
+                        <strong>Due By: </strong>{formatDate(todo.dueDate, true, true)}
+                    </div>
+                    <div className="item">
+                        <strong>Created By: </strong>{todo.createdBy.firstName} {todo.createdBy.lastName}
+                    </div>
+                    <div className="item check">
+                        {/**The check box for completion must go here. */}
+                        <strong>Completed:</strong><Checkbox todo={todo} fetch={props.fetch} />
+                    </div>
+                    <div className="ui inverted mini basic icon buttons" style={{ marginTop: '20px' }}>
+                        <button className="ui button">
+                            <i className="edit icon"></i>
+                        </button>
+                        <button className="ui button">
+                            <i className="trash icon"></i>
+                        </button>
+                    </div>
+                </>
+            ) : (
+                    <div class="ui segment">
+                        <p></p>
+                        <div class="ui active dimmer">
+                            <div class="ui loader"></div>
+                        </div>
+                    </div>
+                )}
         </div >
     )
 }
