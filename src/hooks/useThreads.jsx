e14f98c3bed86522b01b33a1b4a2c4694664791e
jasonhawkharris <jasonhawkharris@gmail.com>
2020-12-08 13:35:40 -0800

    adds custom hook for threads

---
+++ src/hooks/useThreads.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+
+import ThreadModel from '../models/ThreadModel';
+
+const useThreads = (id, query = false) => {
+    const [threads, setThreads] = useState(null);
+
+    const fetchThreads = (id) => {
+        if (!query) {
+            ThreadModel.show(id).then(response => {
+                if (response.msg) setThreads([]);
+                setThreads(response.foundThread);
+            });
+        } else {
+            ThreadModel.all(id).then(response => {
+                if (response.msg) setThreads([]);
+                setThreads(response.foundThreads);
+            });
+        }
+    }
+
+    useEffect(
+        function () {
+            fetchThreads(id)
+            // eslint-disable-next-line react-hooks/exhaustive-deps
+        }, []
+    )
+
+    return [threads, fetchThreads];
+}
+
+export default useThreads;
